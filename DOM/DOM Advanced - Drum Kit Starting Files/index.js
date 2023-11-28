3eda1ef7bcd1125d2a474e9f513cf77d20cf4837
randymz10 <randymz10@gmail.com>
2023-11-28 01:00:02 -0300

    Added new elements

--- DOM/DOM Advanced - Drum Kit Starting Files/index.js
+++ DOM/DOM Advanced - Drum Kit Starting Files/index.js
@@ -1,5 +1,45 @@
-document.querySelectorAll('button').forEach(element => {
-    element.addEventListener('click', () => {
-        alert('I got clicked');
-    });
+document.querySelectorAll("button").forEach((element) => {
+  element.addEventListener("click", () => {
+    const buttonInnerHTML = element.innerHTML;
+    makeSound (buttonInnerHTML);
+  });
 });
+
+document.addEventListener("keydown", (event) => {
+  makeSound(event.key);
+});
+
+const makeSound = (key) => {
+    switch (key) {
+        case "w":
+          const tom1 = new Audio("./sounds/tom-1.mp3");
+          tom1.play();
+          break;
+        case "a":
+          const tom2 = new Audio("./sounds/tom-2.mp3");
+          tom2.play();
+          break;
+        case "s":
+          const tom3 = new Audio("./sounds/tom-3.mp3");
+          tom3.play();
+          break;
+        case "d":
+          const tom4 = new Audio("./sounds/tom-4.mp3");
+          tom4.play();
+          break;
+        case "j":
+          const snare = new Audio("./sounds/snare.mp3");
+          snare.play();
+          break;
+        case "k":
+          const crash = new Audio("./sounds/crash.mp3");
+          crash.play();
+        case "l":
+          const kick = new Audio("./sounds/kick-bass.mp3");
+          kick.play();
+          break;
+        default:
+            console.log(key);
+          break;
+      }
+}
