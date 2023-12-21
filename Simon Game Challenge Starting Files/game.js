63e54b307a443b9df6188928946728f5d32f2cb9
randymz10 <randymz10@gmail.com>
2023-12-20 22:01:14 -0300

    finished project

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,15 +1,74 @@
 const buttonColours = ["red", "blue", "green", "ÿeloww"];
 const gamePattern = [];
+const userClickedPattern = [];
+let started = false;
+let level = 0;
+
+$(document).keypress(function () {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+$(".btn").on("click", function () {
+  const userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound(randomChosenColour);
+});
+
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("sucess");
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    console.log("wrong");
+    playSound("wrong");
+    const body = $("body");
+    body.addClass("game-over");
+    setTimeout(function () {
+      body.removeClass("game-over");
+    }, 200);
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+    startOver();
+  }
+}
 
 function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text(`Level ${level}`);
+
   let randomNumber = Math.floor(Math.random() * 4);
   let randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
   const button = $(`#${randomChosenColour}`);
   button.fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+}
 
-  const sound = new Audio(`./sounds/${randomChosenColour}.mp3`);
+function playSound(name) {
+  const sound = new Audio(`./sounds/${name}.mp3`);
   sound.play();
+}
 
+function animatePress(currentColour) {
+  const currentElement = $(`#${currentColour}`);
+  currentElement.addClass("pressed");
+
+  setTimeout(function () {
+    currentElement.removeClass("pressed");
+  }, 100);
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
 }
