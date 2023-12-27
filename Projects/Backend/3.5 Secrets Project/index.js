082ea5b3d8af43c3018f29e11208a5ab709d2bf1
randymz10 <randymz10@gmail.com>
2023-12-27 14:58:18 -0300

    Added new exercise about EJS

--- Projects/Backend/3.5 Secrets Project/index.js
+++ Projects/Backend/3.5 Secrets Project/index.js
@@ -1,3 +1,42 @@
 //To see how the final website should work, run "node solution.js".
 //Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
+
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+let isUserAuthorized = false;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function checkPassword (req, res, next) {
+    const password = req.body["password"];
+    if(password === 'ILoveProgramming') {
+        isUserAuthorized = true;
+    }
+    next();
+};
+
+app.use(checkPassword);
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/check", (req, res) =>{
+    if (isUserAuthorized) {
+        res.sendFile(`${__dirname}/public/secret.html`);
+    } else {
+        res.sendFile(__dirname + "/public/index.html");
+    }
+});
+
+app.listen(port, () => {
+  console.log(`App running on port: ${port}`);
+});
