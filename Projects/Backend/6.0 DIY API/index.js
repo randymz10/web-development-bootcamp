e43e58464cb2fd9fb6bcf1b73a4eaecfd9da737f
randymz10 <randymz10@gmail.com>
2024-01-08 22:59:26 -0300

    Added two new exercises

--- Projects/Backend/6.0 DIY API/index.js
+++ Projects/Backend/6.0 DIY API/index.js
@@ -31,13 +31,76 @@ app.get('/filter', (req, res)=>{
 });
 //4. POST a new joke
 
+app.post('/jokes', (req, res) => {
+  try {
+    const newJoke = {
+      id: jokes.length + 1,
+      jokeText: req.body.text,
+      jokeType: req.body.type,
+    };
+
+    jokes.push(joke)
+    res.status(200).json(newJoke);
+  } catch (error) {
+    console.error(error);
+    res.status(500);
+  }
+});
+
 //5. PUT a joke
+
+app.put('jokes/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const joke = jokes.find((joke) => joke.id === id);
+  joke.jokeText = req.body.text;
+  joke.jokeType = req.body.type;
+  res.status(200).json(joke);
+});
 
 //6. PATCH a joke
 
+app.patch('/jokes/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const existingJoke = jokes.find((joke) => joke.id === id);
+  const replacementJoke = {
+    id: id,
+    jokeText: req.body.text || existingJoke.jokeText,
+    jokeType: req.body.type || existingJoke.jokeType,
+  };
+  const searchIndex = jokes.findIndex((joke) => joke.id === id);
+  jokes[searchIndex] = replacementJoke;
+  console.log(jokes[searchIndex]);
+  res.json(replacementJoke);
+});
+
 //7. DELETE Specific joke
 
+app.delete("/jokes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const searchIndex = jokes.findIndex((joke) => joke.id === id);
+  if (searchIndex > -1) {
+    jokes.splice(searchIndex, 1);
+    res.sendStatus(200);
+  } else {
+    res
+      .status(404)
+      .json({ error: `Joke with id: ${id} not found. No jokes were deleted.` });
+  }
+});
+
 //8. DELETE All jokes
+
+app.delete("/all", (req, res) => {
+  const userKey = req.query.key;
+  if (userKey === masterKey) {
+    jokes = [];
+    res.sendStatus(200);
+  } else {
+    res
+      .status(404)
+      .json({ error: `You are not authorised to perform this action.` });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Successfully started server on port ${port}.`);
