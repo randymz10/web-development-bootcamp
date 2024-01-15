55b37a02d5a2ad5d2c83c7fb450965dbb5c46318
randymz10 <randymz10@gmail.com>
2024-01-15 01:42:14 -0300

    Added new exercises

--- Projects/Backend/Databases/8.3 Travel Tracker/index.js
+++ Projects/Backend/Databases/8.3 Travel Tracker/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import { render } from "ejs";
 
 const app = express();
 const port = 3000;
@@ -17,34 +18,60 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 db.connect();
 
-app.get("/", async (req, res) => {
-  // Write your code here.
+const getCountries = async () => {
   let countries = "";
   const result = await db.query("SELECT * FROM visited_countries");
   result.rows.forEach((country) => {
     countries += `${country.country_code},`;
   });
-  
+  return { countries: countries, total: result.rowCount };
+};
+
+app.get("/", async (req, res) => {
+  // Write your code here.
+  const countries = getCountries();
+
   res.render("index.ejs", {
-    countries: countries,
-    total: result.rowCount,
+    countries: (await countries).countries,
+    total: (await countries).total,
   });
 });
 
-app.post("/add", async(req, res) => {
+app.post("/add", async (req, res) => {
   try {
-    const visitedCountry = await db.query(`
-    SELECT country_code
-    FROM countries 
-    WHERE country_name = $1`, [req.body.country]);
-    
-    await db.query('INSERT INTO visited_countries (country_code) VALUES ($1)', [visitedCountry.rows[0].country_code]);
+    const visitedCountry = await db.query(
+      `
+    SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%'`,
+      [req.body.country.toLowerCase()]
+    );
 
-    res.redirect('/');
-    
+    if (visitedCountry.rowCount > 0) {
+      try {
+        await db.query(
+          "INSERT INTO visited_countries (country_code) VALUES ($1)",
+          [visitedCountry.rows[0].country_code]
+        );
+        res.redirect('/');
+      } catch (error) {
+        console.error(error);
+        const countries = getCountries();
+        res.render("index.ejs", {
+          error: "Country has already been added, try again",
+          countries: (await countries).countries,
+          total: (await countries).total,
+        });
+      }
+    } else {
+      const countries = getCountries();
+      res.render("index.ejs", {
+        error: "Country does not exit, try again",
+        countries: (await countries).countries,
+        total: (await countries).total,
+      });
+      res.status(500);
+    }
   } catch (error) {
     console.error(error);
-    res.status(500).send('Internal error');
   }
 });
 
